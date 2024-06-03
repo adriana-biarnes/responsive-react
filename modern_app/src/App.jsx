9ceda58b1dd02ff7b1918394c154d1a42d5bec6a
Adriana Biarnés García <adrianabiarnesgarcia@MacBook-Air-de-Adriana.local>
2024-06-03 12:06:35 +0200

    initial commit

---
+++ modern_app/src/App.jsx
@@ -0,0 +1,40 @@
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Testimonials,
+  Footer,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}><Navbar /></div>
+    </div>
+
+    <div className={`$bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}><Hero /></div>
+    </div>
+
+    <div className={`$bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Clients />
+        <CTA />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
